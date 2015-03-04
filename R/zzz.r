f92a6a70676afda7f5c7af8d8d4ddcc5a104bd8f
JED <jedazard@users.noreply.github.com>
2015-03-04 18:48:38 -0500

    R Package R Codes

--- R/zzz.r
+++ R/zzz.r
@@ -5,11 +5,6 @@
 .onAttach <- function(libname, pkgname) {
     SSver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname), 
                       fields="Version")
-    packageStartupMessage("\n")
     packageStartupMessage(paste(pkgname, SSver))
-    packageStartupMessage("\n")
-    packageStartupMessage("\n")
-    packageStartupMessage("Type MVR.news() to see new features, changes, and bug fixes.")
-    packageStartupMessage("\n")
-    packageStartupMessage("\n")
+    packageStartupMessage("Type MVR.news() to see new features, changes, and bug fixes\n")
 }
